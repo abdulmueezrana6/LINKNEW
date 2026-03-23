2c74e36abf11a88d2c88217645ef76ca6ec36e6f
sellxilips <sellxilips@gmail.com>
2026-03-23 13:54:09 +0700

    commit

--- src/components/GoogleTranslate.jsx
+++ src/components/GoogleTranslate.jsx
@@ -2,42 +2,53 @@ import React, { useEffect } from "react";
 
 const GoogleTranslate = () => {
   useEffect(() => {
+    // 1. Tạo script Google Translate
     const script = document.createElement("script");
     script.src =
       "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
     script.async = true;
     document.body.appendChild(script);
 
+    // 2. Hàm callback khi Google Translate load xong
     window.googleTranslateElementInit = () => {
       new window.google.translate.TranslateElement(
         {
-          pageLanguage: "en", 
-          autoDisplay: false, 
+          pageLanguage: "en",
+          autoDisplay: false,
         },
-        "google_translate_element" 
+        "google_translate_element"
       );
-      setTimeout(waitForWidgetLoad, 1000); 
+
+      // 3. Chờ dropdown xuất hiện bằng MutationObserver
+      waitForWidgetLoad();
     };
+
     return () => {
       document.body.removeChild(script);
     };
   }, []);
 
+  // 4. Hàm chờ widget load
   const waitForWidgetLoad = () => {
-  const observer = new MutationObserver(() => {
-    const select = document.querySelector(".goog-te-combo");
-    if (select) {
-      const location = JSON.parse(localStorage.getItem("location"));
-      const userLang = location.lang;
-      if (select.value !== userLang) {
-        select.value = userLang;
-        select.dispatchEvent(new Event("change", { bubbles: true }));
+    const observer = new MutationObserver(() => {
+      const select = document.querySelector(".goog-te-combo");
+      if (select) {
+        const location = JSON.parse(localStorage.getItem("location") || "{}");
+        const userLang = location.lang;
+
+        if (userLang && select.value !== userLang) {
+          select.value = userLang;
+
+          // iOS cần event có bubbles để trigger Google Translate
+          select.dispatchEvent(new Event("change", { bubbles: true }));
+        }
+
+        observer.disconnect(); // Dừng quan sát khi đã set ngôn ngữ
       }
-      observer.disconnect();
-    }
-  });
-  observer.observe(document.body, { childList: true, subtree: true });
-};
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  };
 
   return <div id="google_translate_element"></div>;
 };
